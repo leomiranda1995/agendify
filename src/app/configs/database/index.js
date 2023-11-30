1d372529ca2374214134c7d03a200debdbc6a16e
Leonardo da Silva Miranda <leomiranda1995@gmail.com>
2023-11-30 16:19:41 -0300

    add bd online add dot env

--- src/app/configs/database/index.js
+++ src/app/configs/database/index.js
@@ -1,11 +1,9 @@
 const { Client } = require('pg');
+require('dotenv').config();
 
 const client = new Client({
-  host: 'localhost',
-  port: 5432,
-  user: 'root',
-  password: 'root',
-  database: 'agendify',
+  connectionString: process.env.DB_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 client.connect();
