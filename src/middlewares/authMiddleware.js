1183a6a88181b1dc0d4f435b39dd6e779905e8a6
Leonardo da Silva Miranda <leomiranda1995@gmail.com>
2023-12-01 17:54:09 -0300

    removida lógica da autenticação para teste do front agora sem ela.

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,20 +1,20 @@
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const authMiddleware = (req, res, next) => {
-  const token = req.header('Authorization');
+  // const token = req.header('Authorization');
 
-  if (!token) {
-    return res.status(401).json({ message: 'Token não fornecido' });
-  }
+  // if (!token) {
+  //   return res.status(401).json({ message: 'Token não fornecido' });
+  // }
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.userId;
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: 'Token inválido' });
-  }
+  // try {
+  //   const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  //   req.userId = decoded.userId;
+  next();
+  // } catch (error) {
+  //   return res.status(401).json({ message: 'Token inválido' });
+  // }
 };
 
 module.exports = authMiddleware;
